927fa4dee8506a7e53e21d78cdc32c94a455fe37
Yoan THIRION <ythirion@agilepartner.net>
2019-10-31 13:51:19 +0100

    Simplify validator

--- src/main/java/PersonValidator.java
+++ src/main/java/PersonValidator.java
@@ -1,52 +1,21 @@
 import lombok.NonNull;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PersonValidator {
+    private static final String PHONE_NUMBER_REGEX = "^[0-9]{1,10}$";
+    private static final String[] INVALID_TOYS = new String[] { "Spongebob", "Hello Kitty" };
+
     public List<String> validate(@NonNull Person person) {
         List<String> constraints = new ArrayList<String>();
 
         check(person.getPostalCode(), "postal code", constraints);
         check(person.getName(), "name", constraints);
-
-        if (person.getPhoneNumbers() != null && person.getPhoneNumbers().length > 0) {
-            for (String phoneNumber : person.getPhoneNumbers()) {
-                if (phoneNumber != null) {
-                    if (phoneNumber.isEmpty()) {
-                        constraints.add("Error - Phone number is empty");
-                        return constraints;
-                    } else {
-                        if (phoneNumber.length() < 10) {
-                            constraints.add("Error - Invalid phone number");
-                            return constraints;
-                        }
-                    }
-
-                } else {
-                    constraints.add("Error - Invalid phone number");
-                    return constraints;
-                }
-            }
-        }else constraints.add("Error - No phone number supplied");
-
-        if (person instanceof Kid) {
-            Kid kid = (Kid) person;
-            if (kid.getFavoriteToy() != null) {
-                if (kid.getFavoriteToy().isEmpty()) {
-                    constraints.add("Error - No favorite toy for kid");
-                } else {
-                    switch (kid.getFavoriteToy()) {
-                        case "Spongebob":
-                            constraints.add("Error - Spongebob is not allowed as favoriteToy");
-                        case "Hello Kitty":
-                            constraints.add("Error - Hello Kitty is not allowed as favoriteToy");
-                    }
-                }
-            } else {
-
-            }
-        }
+        checkPhoneNumbers(person, constraints);
+        checkFavoriteToy(person, constraints);
 
         if (constraints.size() > 0) {
             constraints.add("Error - Validation errors have been found");
@@ -55,9 +24,38 @@ public class PersonValidator {
         return constraints;
     }
 
+    private void checkFavoriteToy(Person person, List<String> constraints) {
+        if (person instanceof Kid) {
+            Kid kid = (Kid) person;
+
+            check(kid.getFavoriteToy(), "favorite toy", constraints);
+
+            if(Arrays.stream(INVALID_TOYS).anyMatch(t -> t == kid.getFavoriteToy())){
+                constraints.add("Error - " + kid.getFavoriteToy() + " is not allowed as favoriteToy");
+            }
+        }
+    }
+
+    private void checkPhoneNumbers(Person person, List<String> constraints) {
+        if (person.getPhoneNumbers() == null ||
+                person.getPhoneNumbers().length == 0){
+            constraints.add("Error - No phone number supplied");
+        } else {
+            boolean containsValidPhoneNumber =
+                    Arrays.stream(person.getPhoneNumbers())
+                            .filter(phoneNumber -> phoneNumber != null &&
+                                    !phoneNumber.isEmpty() &&
+                                    phoneNumber.matches(PHONE_NUMBER_REGEX))
+                            .count() > 0;
+
+            if(!containsValidPhoneNumber){
+                constraints.add("Error - Invalid phone number");
+            }
+        }
+    }
+
     private void check (String fieldValue, String fieldName, List < String > constraints){
-        if (fieldValue == null ||
-                fieldValue.isEmpty()) {
+        if (fieldValue == null || fieldValue.isEmpty()) {
             constraints.add("Error - " + fieldName + " is not defined for the person");
         }
     }
